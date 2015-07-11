f71d34d9e5c863f1b600f67b0ba79064abc516f3
ralem1234 <ralem1234@yahoo.com>
2015-07-10 23:39:35 -0500

    JAVA CODE APPLIED

--- app/src/main/java/com/example/ralemproductions/ezchat/MainActivity.java
+++ app/src/main/java/com/example/ralemproductions/ezchat/MainActivity.java
@@ -4,10 +4,10 @@ import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -23,7 +23,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ClickButton("8608965042");
         callText = (TextView)findViewById(R.id.call);
         UpdateCallText();
     }
@@ -35,7 +34,7 @@ public class MainActivity extends Activity {
 
     public void BackSpace(){
         if(currentNumber.length() <= 0){return;}
-        currentNumber.substring(0, currentNumber.length() - 1);
+        currentNumber = currentNumber.substring(0, currentNumber.length() - 1);
         UpdateCallText();
     }
     public void UpdateCallText(){
@@ -53,46 +52,46 @@ public class MainActivity extends Activity {
         }
     }
 
-    public void Button1() {
+    public void Button1(View view) {
         ClickButton("1");
     }
-    public void Button2() {
+    public void Button2(View view) {
         ClickButton("2");
     }
-    public void Button3() {
+    public void Button3(View view) {
         ClickButton("3");
     }
-    public void Button4() {
+    public void Button4(View view) {
         ClickButton("4");
     }
-    public void Button5() {
+    public void Button5(View view) {
         ClickButton("5");
     }
-    public void Button6() {
+    public void Button6(View view) {
         ClickButton("6");
     }
-    public void Button7() {
+    public void Button7(View view) {
         ClickButton("7");
     }
-    public void Button8() {
+    public void Button8(View view) {
         ClickButton("8");
     }
-    public void Button9() {
+    public void Button9(View view) {
         ClickButton("9");
     }
-    public void Button0() {
+    public void Button0(View view) {
         ClickButton("0");
     }
-    public void ButtonPound() {
+    public void ButtonPound(View view) {
         ClickButton("#");
     }
-    public void ButtonStar() {
+    public void ButtonStar(View view) {
         ClickButton("1");
     }
-    public void ButtonDelete(){
+    public void ButtonDelete(View view){
         BackSpace();
     }
-    public void ButtonCall(){
+    public void ButtonCall(View view){
         Call();
     }
 }
