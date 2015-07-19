768081457fd62b20f5d2f304d7fa4e77212b2466
joshtrance <joshtrance@gmail.com>
2015-07-18 20:32:16 -0500

    reconfigured

--- app/src/main/java/com/example/ralemproductions/ezchat/caller.java
+++ app/src/main/java/com/example/ralemproductions/ezchat/caller.java
@@ -6,18 +6,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class Title extends ActionBarActivity {
+public class caller extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_title);
+        setContentView(R.layout.activity_caller);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_title, menu);
+        getMenuInflater().inflate(R.menu.menu_caller, menu);
         return true;
     }
 
